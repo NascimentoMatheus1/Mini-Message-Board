f8b335345a7354ef90fe19e017b8224d7bc47184
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-01-29 21:54:12 -0300

    refactor: update controller to use db functions

--- controllers/messageControler.js
+++ controllers/messageControler.js
@@ -1,38 +1,50 @@
+const db = require('../db/queries');
 const { body, validationResult, matchedData } = require('express-validator');
 
-let messages = [
-    {
-        id: 0,
-        text: 'Hi there!',
-        user: 'Amando',
-        added: new Date(),
-        detailsLink: '/messages/0',
-    },
-    {
-        id: 1,
-        text: 'Hello World!',
-        user: 'Charles',
-        added: new Date(),
-        detailsLink: '/messages/1',
-    },
-];
-
-exports.getAllMessages = (req, res) => {
-    res.render('index', { title: 'Mini Messageboard', messages });
-};
-
-exports.getMessageById = (req, res) => {
-    const { id } = req.params;
-
-    if (!messages[id]) {
-        res.render('404', { title: 'Error 404' });
-        return;
+const getAllMessages = async (req, res) => {
+    try {
+        const messages = await db.getAllMessages();
+        res.render('index', { title: 'Mini Messageboard', messages });
+    } catch (error) {
+        console.log(error.message);
+        serverError(res);
     }
-
-    res.render('messageDetail', { title: 'Details', message: messages[id] });
 };
 
-exports.getFormPage = (req, res) => {
+const getMessageById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (isNaN(Number(id))) {
+            res.status(400).render('error', {
+                title: 'Error 400',
+                errorCode: '400',
+                errorMessage: ' Bad Request ',
+                errorDetails: 'Request params could not be read properly.',
+            });
+            return;
+        }
+
+        const result = await db.getMessageById(Number(id));
+        if (!result) {
+            res.status(404).render('error', {
+                title: 'Error 404',
+                errorCode: '404',
+                errorMessage: 'Message Not Found ',
+                errorDetails:
+                    "The Message you're looking for was moved, deleted, or never existed in our board.",
+            });
+            return;
+        }
+
+        res.render('messageDetail', { title: 'Details', item: result });
+    } catch (error) {
+        console.log(error.message);
+        serverError(res);
+    }
+};
+
+const getFormPage = async (req, res) => {
     res.render('form', { title: 'new message' });
 };
 
@@ -51,31 +63,55 @@ const validadeNewMessage = [
         .withMessage('Message is too long (max 300 chars)'),
 ];
 
-exports.messageCreatePost = [
+const messageCreatePost = [
     validadeNewMessage,
-    (req, res) => {
-        const errors = validationResult(req);
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            return res.status(400).render('form', {
-                title: 'Bad Request',
-                errors: errors.array(),
-            });
+            if (!errors.isEmpty()) {
+                return res.status(400).render('form', {
+                    title: 'Bad Request',
+                    errors: errors.array(),
+                });
+            }
+
+            const { userName, userMessage } = matchedData(req);
+            await db.insertMessage(userName, userMessage);
+            res.redirect('/');
+        } catch (error) {
+            console.log(error.message);
+            serverError(res);
         }
-
-        const { userMessage, userName } = matchedData(req);
-        let newId = messages[messages.length - 1].id + 1;
-        messages.push({
-            id: newId,
-            text: userMessage,
-            user: userName,
-            added: new Date(),
-            detailsLink: `/messages/${newId}`,
-        });
-        res.redirect('/');
     },
 ];
 
-exports.getErrorPage = (req, res) => {
-    res.render('404', { title: 'Error 404' });
+const getErrorPage = async (req, res) => {
+    res.status(404).render('error', {
+        title: 'Error 404',
+        errorCode: '404',
+        errorMessage: 'Page Not Found ',
+        errorDetails:
+            "The page you're looking for was moved, deleted, or never existed in our board.",
+    });
+    return;
+};
+
+function serverError(res) {
+    res.status(500).render('error', {
+        title: 'Error 500',
+        errorCode: '500',
+        errorMessage: ' Internal Server Error !',
+        errorDetails:
+            'The server encountered an internal error or misconfiguration and was unable to complete your request.',
+    });
+    return;
+}
+
+module.exports = {
+    getAllMessages,
+    getMessageById,
+    getFormPage,
+    getErrorPage,
+    messageCreatePost,
 };
