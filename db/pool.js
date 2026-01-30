de675dcd0fe0277c97cc123b6de8ada37b8c9c7b
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-01-29 22:29:52 -0300

    chore(deploy): configure postgres database on Neon

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// module.exports = new Pool({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     database: process.env.DATABASE,
+//     password: process.env.PASSWORD,
+//     port: 5432,
+// });
+
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
 });
